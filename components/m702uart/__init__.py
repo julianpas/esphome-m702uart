38f7b4cc4524e93d3799f13242df9a335e66fc34
Julian Pastarmov <pastarmovj@gmail.com>
2025-06-25 10:29:13 +0200

    Update __init__.py

--- components/m702uart/__init__.py
+++ components/m702uart/__init__.py
@@ -1,35 +1,38 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor, uart
+from esphome.components import sensor, binary_sensor, text_sensor, uart
 from esphome.const import (
     CONF_ID,
     CONF_TEMPERATURE,
     CONF_HUMIDITY,
     DEVICE_CLASS_CARBON_DIOXIDE,
     DEVICE_CLASS_HUMIDITY,
+    DEVICE_CLASS_OZONE,
     DEVICE_CLASS_PM10,
     DEVICE_CLASS_PM25,
     DEVICE_CLASS_TEMPERATURE,
-    DEVICE_CLASS_VOLATILE_ORGANIC_COMPOUNDS_PARTS,
+    DEVICE_CLASS_VOLATILE_ORGANIC_COMPOUNDS,
+    DEVICE_CLASS_CONNECTIVITY,
     STATE_CLASS_MEASUREMENT,
-    UNIT_CELSIUS,
-    UNIT_PARTS_PER_BILLION,
     UNIT_PARTS_PER_MILLION,
+    UNIT_PARTS_PER_BILLION,
+    UNIT_MICROGRAM_PER_CUBIC_METER,
+    UNIT_CELSIUS,
     UNIT_PERCENT,
 )
 
 DEPENDENCIES = ["uart"]
 
-n702b_ns = cg.esphome_ns.namespace("n702b")
-N702BComponent = n702b_ns.class_("N702BComponent", cg.Component, uart.UARTDevice)
-
 CONF_ECO2 = "eco2"
-CONF_O3 = "o3"
+CONF_OZONE = "ozone"
 CONF_TVOC = "tvoc"
-CONF_PM25 = "pm25"
-CONF_PM10 = "pm10"
+CONF_PM_2_5 = "pm_2_5"
+CONF_PM_10 = "pm_10"
+CONF_DATA_VALID = "data_valid"
+CONF_SENSOR_STATUS = "sensor_status"
 
-UNIT_MICROGRAMS_PER_CUBIC_METER = "µg/m³"
+m702uart_ns = cg.esphome_ns.namespace("m702uart")
+M702UARTComponent = m702uart_ns.class_("M702UARTComponent", cg.PollingComponent, uart.UARTDevice)
 
 CONFIG_SCHEMA = (
     cv.Schema(
@@ -41,44 +44,49 @@ CONFIG_SCHEMA = (
                 device_class=DEVICE_CLASS_CARBON_DIOXIDE,
                 state_class=STATE_CLASS_MEASUREMENT,
             ),
-            cv.Optional(CONF_O3): sensor.sensor_schema(
+            cv.Optional(CONF_OZONE): sensor.sensor_schema(
                 unit_of_measurement=UNIT_PARTS_PER_BILLION,
                 accuracy_decimals=0,
+                device_class=DEVICE_CLASS_OZONE,
                 state_class=STATE_CLASS_MEASUREMENT,
             ),
             cv.Optional(CONF_TVOC): sensor.sensor_schema(
                 unit_of_measurement=UNIT_PARTS_PER_BILLION,
                 accuracy_decimals=0,
-                device_class=DEVICE_CLASS_VOLATILE_ORGANIC_COMPOUNDS_PARTS,
+                device_class=DEVICE_CLASS_VOLATILE_ORGANIC_COMPOUNDS,
                 state_class=STATE_CLASS_MEASUREMENT,
             ),
-            cv.Optional(CONF_PM25): sensor.sensor_schema(
-                unit_of_measurement=UNIT_MICROGRAMS_PER_CUBIC_METER,
+            cv.Optional(CONF_PM_2_5): sensor.sensor_schema(
+                unit_of_measurement=UNIT_MICROGRAM_PER_CUBIC_METER,
                 accuracy_decimals=0,
                 device_class=DEVICE_CLASS_PM25,
                 state_class=STATE_CLASS_MEASUREMENT,
             ),
-            cv.Optional(CONF_PM10): sensor.sensor_schema(
-                unit_of_measurement=UNIT_MICROGRAMS_PER_CUBIC_METER,
+            cv.Optional(CONF_PM_10): sensor.sensor_schema(
+                unit_of_measurement=UNIT_MICROGRAM_PER_CUBIC_METER,
                 accuracy_decimals=0,
                 device_class=DEVICE_CLASS_PM10,
                 state_class=STATE_CLASS_MEASUREMENT,
             ),
             cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
                 unit_of_measurement=UNIT_CELSIUS,
-                accuracy_decimals=1,
+                accuracy_decimals=2,
                 device_class=DEVICE_CLASS_TEMPERATURE,
                 state_class=STATE_CLASS_MEASUREMENT,
             ),
             cv.Optional(CONF_HUMIDITY): sensor.sensor_schema(
                 unit_of_measurement=UNIT_PERCENT,
-                accuracy_decimals=1,
+                accuracy_decimals=2,
                 device_class=DEVICE_CLASS_HUMIDITY,
                 state_class=STATE_CLASS_MEASUREMENT,
             ),
+            cv.Optional(CONF_DATA_VALID): binary_sensor.binary_sensor_schema(
+                device_class=DEVICE_CLASS_CONNECTIVITY,
+            ),
+            cv.Optional(CONF_SENSOR_STATUS): text_sensor.text_sensor_schema(),
         }
     )
-    .extend(cv.COMPONENT_SCHEMA)
+    .extend(cv.polling_component_schema("30s"))
     .extend(uart.UART_DEVICE_SCHEMA)
 )
 
@@ -92,21 +100,21 @@ async def to_code(config):
         sens = await sensor.new_sensor(config[CONF_ECO2])
         cg.add(var.set_eco2_sensor(sens))
 
-    if CONF_O3 in config:
-        sens = await sensor.new_sensor(config[CONF_O3])
-        cg.add(var.set_o3_sensor(sens))
+    if CONF_OZONE in config:
+        sens = await sensor.new_sensor(config[CONF_OZONE])
+        cg.add(var.set_ozone_sensor(sens))
 
     if CONF_TVOC in config:
         sens = await sensor.new_sensor(config[CONF_TVOC])
         cg.add(var.set_tvoc_sensor(sens))
 
-    if CONF_PM25 in config:
-        sens = await sensor.new_sensor(config[CONF_PM25])
-        cg.add(var.set_pm25_sensor(sens))
+    if CONF_PM_2_5 in config:
+        sens = await sensor.new_sensor(config[CONF_PM_2_5])
+        cg.add(var.set_pm_2_5_sensor(sens))
 
-    if CONF_PM10 in config:
-        sens = await sensor.new_sensor(config[CONF_PM10])
-        cg.add(var.set_pm10_sensor(sens))
+    if CONF_PM_10 in config:
+        sens = await sensor.new_sensor(config[CONF_PM_10])
+        cg.add(var.set_pm_10_sensor(sens))
 
     if CONF_TEMPERATURE in config:
         sens = await sensor.new_sensor(config[CONF_TEMPERATURE])
@@ -115,3 +123,11 @@ async def to_code(config):
     if CONF_HUMIDITY in config:
         sens = await sensor.new_sensor(config[CONF_HUMIDITY])
         cg.add(var.set_humidity_sensor(sens))
+
+    if CONF_DATA_VALID in config:
+        sens = await binary_sensor.new_binary_sensor(config[CONF_DATA_VALID])
+        cg.add(var.set_data_valid_binary_sensor(sens))
+
+    if CONF_SENSOR_STATUS in config:
+        sens = await text_sensor.new_text_sensor(config[CONF_SENSOR_STATUS])
+        cg.add(var.set_sensor_status_text_sensor(sens))
